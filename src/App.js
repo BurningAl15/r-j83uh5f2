b74f195a253d3c1fd3ffb3bb5d5f7194a8df587a
Aldhair Vera <aldhairvera15@gmail.com>
2022-01-04 15:51:16 -0500

    fix: counting characters from textarea and setting the size of it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, isValidElement } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: ""
+    }
+  }
+
+  count = (e) => {
+    this.setState({
+      counter: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.count}></textarea>
+        <div className="counter">{this.state.counter.length}</div>
       </div>
     );
   }
